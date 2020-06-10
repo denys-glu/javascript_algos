05947987d12ebb6f7394f29f881b43ed841e296f
oggyman <oggyla@gmail.com>
2020-06-10 10:29:43 -0700

    mergin sort helper function to merge sorted arrays

---
+++ Sorting_Merge.js
@@ -0,0 +1,24 @@
+function mergingArrays(arr1, arr2) {
+    let arr = [];
+    let j = 0;
+    let i = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] > arr2[j]) {
+            arr.push(arr2[j]);
+            j++;
+        } else {
+            arr.push(arr1[i])
+            i++;
+        }
+    }
+    while (i < arr1.length) {
+        arr.push(arr1[i])
+        i++;
+    }
+    while (j < arr2.length) {
+        arr.push(arr2[j])
+        j++;
+    }
+    return arr;
+}
